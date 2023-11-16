f2b50e0152c2ffc831e071d060cceefef94af715
Daniel Szalkowski <chenano89@gmail.com>
2023-11-16 11:31:20 +0100

    version noob 3

--- app/src/main/java/com/pmdm/appimc/IMCCalculatorActivity.kt
+++ app/src/main/java/com/pmdm/appimc/IMCCalculatorActivity.kt
@@ -2,14 +2,19 @@ package com.pmdm.appimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.slider.RangeSlider
+import java.text.DecimalFormat
 
 class IMCCalculatorActivity : AppCompatActivity() {
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale:CardView
     private var isMale: Boolean = true
+    private lateinit var rsHeight: RangeSlider
+    private lateinit var tvHeight: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imccalculator)
@@ -20,6 +25,8 @@ class IMCCalculatorActivity : AppCompatActivity() {
     private fun initComponents() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+        rsHeight = findViewById(R.id.rsHeight)
+        tvHeight = findViewById(R.id.tvHeight)
     }
     private fun initListeners(){
         viewMale.setOnClickListener(){
@@ -30,8 +37,12 @@ class IMCCalculatorActivity : AppCompatActivity() {
             isMale=false
             setGenderColor()
         }
-
+        rsHeight.addOnChangeListener { _, value, _ ->
+            //tvHeight.text = value.toString()
+            tvHeight.text = DecimalFormat("#.##").format(value) + " cm"
+        }
     }
+
     private fun setGenderColor(){
         viewMale.setCardBackgroundColor(getBackgroundColor(isMale))
         viewFemale.setCardBackgroundColor(getBackgroundColor(!isMale))
